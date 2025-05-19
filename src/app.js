5b97c97c185ffc7408170c2ae9eabd9482f41b7f
Rishabh Tyagi <RTyagi+flexera@flexera.com>
2025-05-19 17:50:00 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -1,30 +1,108 @@
 const express = require('express');
+const validator = require('validator');
 const connectDB = require('./config/database')
 
 const app = express();
 const User = require("./models/user");
 
-app.post('/signup', async(req, res) => {
-    const user = new User({
-        firstName: "Rishabh",
-        lastName: "Tyagi",
-        emailId: "rishabhtyagi21.rt@gmail.com",
-        password: '987788'
-    })
+app.use(express.json())
+
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body)
     try {
         await user.save();
         res.send('user added successfully!!')
-    } catch(err){
+    } catch (err) {
         res.status(500).send("Error saving the user:", err.message)
     }
 })
 
-connectDB().then(()=>{
+app.get('/users', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send('User Not Found');
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send('Something Went Wrong!');
+    }
+})
+
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.findOne({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send('User Not Found');
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send('Something Went Wrong!');
+    }
+})
+
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send('User Deleted Sucessfully')
+    } catch (err) {
+        res.status(400).send('Something Went Wrong!');
+    }
+})
+
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+
+    try {
+        const ALLOWED_UPDATES = [
+            'photoUrl',
+            'about',
+            'gender',
+            'age',
+            'skills',
+            'userId'
+        ]
+
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+        if (!isUpdateAllowed) {
+            throw new Error("Update not allowed!")
+        }
+        await User.findByIdAndUpdate({ _id: userId }, data, {
+            runValidators: true
+        });
+        res.send('User Updated Successfully!')
+    } catch (err) {
+        res.status(400).send('Update Failed: '+ err.message)
+    }
+})
+
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            res.status(404).send('Users Not Found');
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send('Something Went Wrong!');
+    }
+})
+
+connectDB().then(() => {
     console.log('Database connection established...');
-    app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    })
 })
-})
-.catch((err)=>{
-    console.error("Database cannot be connected!!")
-})
+    .catch((err) => {
+        console.error("Database cannot be connected!!")
+    })
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,67 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+       validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password: " + value);
+        }
+      },
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        enum: {
+            values: ['male', 'female', 'other'],
+            message: `{value} is not a valid gender type`
+        }
     },
+    isPremium: {
+        type: Boolean,
+        default: false,
+    },
+    photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
+    skills: {
+      type: [String],
+    },
+},{
+    timestamps: true
 })
 
 const userModel = mongoose.model('User', userSchema)
