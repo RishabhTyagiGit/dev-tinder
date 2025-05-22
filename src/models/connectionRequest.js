45390f8a09b783a9a12038ffbf276504e9cf8934
Rishabh Tyagi <RTyagi+flexera@flexera.com>
2025-05-22 15:38:56 +0530

    Changes for connection request api

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ['ignored', 'interested', 'accepted', 'rejected'],
+            message: `{VALUE} is incorrect status type`,
+        }
+    }
+},
+    { timestamps: true });
+//It is combined indexing, since we are searching both together in one of the use cases
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
+
+connectionRequestSchema.pre('save', function (next) {
+    const connectionRequest = this;
+    // Check if the fromUserId is same as toUserId
+    // we compare id with equal method since it is not simple string
+    if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("Cannot send connection request to yourself!");
+    }
+    next();
+})
+
+const connectionRequestModel = new mongoose.model('connectRequest', connectionRequestSchema)
+
+module.exports = connectionRequestModel;
